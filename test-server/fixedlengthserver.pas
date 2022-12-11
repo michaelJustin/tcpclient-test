bba44a74ff446918456ae4601dea2da51de9af87
Michael Justin <michael@mikejustin.com>
2022-12-11 12:41:30 +0100

    fixed length server

---
+++ test-server/fixedlengthserver.pas
@@ -0,0 +1,57 @@
+(*
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*)
+
+unit FixedLengthServer;
+
+interface
+
+const
+  SERVER_PORT = 30000;
+  CONTENT_LENGTH = 8192;
+
+procedure Run;
+
+implementation
+
+uses
+  IdCustomTCPServer, IdContext, IdGlobal, SysUtils, Classes;
+
+type
+  { TFixedLengthServer }
+
+  TFixedLengthServer = class(TIdCustomTCPServer)
+  protected
+    function DoExecute(AContext: TIdContext): boolean; override;
+  end;
+
+{ TFixedLengthServer }
+
+function TFixedLengthServer.DoExecute(AContext: TIdContext): boolean;
+var
+  Buffer: TIdBytes;
+begin
+  Result := inherited;
+  Buffer := ToBytes(StringOfChar('A', CONTENT_LENGTH));
+  AContext.Connection.IOHandler.WriteDirect(Buffer, CONTENT_LENGTH);
+  AContext.Connection.Disconnect;
+end;
+
+procedure Run;
+var
+  ExampleServer: TIdCustomTCPServer;
+begin
+  ExampleServer := TFixedLengthServer.Create;
+  ExampleServer.DefaultPort := SERVER_PORT;
+  ExampleServer.Active := True;
+
+  WriteLn(Format('server is listening on port %d', [30000]));
+  WriteLn('hit any key to stop');
+  ReadLn;
+
+  ExampleServer.Active := False;
+end;
+
+end.
